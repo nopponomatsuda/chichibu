5e17c52fa66431df0f47337c59472e7750be2c83
matsuda-naoyuki-1984 <itowokashirecords@gmail.com>
2019-08-30 15:48:21 +0900

    enable to login using aws congnite

---
+++ app/src/main/java/com/matsuda/chichibu/AuthenticatorActivity.kt
@@ -0,0 +1,50 @@
+package com.matsuda.chichibu
+
+import android.os.Bundle
+import android.app.Activity
+import android.util.Log
+import android.content.Intent
+import com.amazonaws.mobile.client.*
+
+class AuthenticatorActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authenticator)
+
+        AWSMobileClient.getInstance().initialize(this, object : Callback<UserStateDetails> {
+            override fun onResult(userStateDetails: UserStateDetails) {
+                Log.d("showSignIn", userStateDetails.userState.name)
+                when (userStateDetails.userState) {
+                    UserState.SIGNED_IN -> signInSuccess()
+                    UserState.SIGNED_OUT -> showSignIn()
+                    else -> AWSMobileClient.getInstance().signOut()
+                }
+            }
+
+            override fun onError(e: Exception) {
+                Log.e("initialize", "Error during initialization", e)
+            }
+        })
+    }
+
+    private fun signInSuccess() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun showSignIn() {
+        AWSMobileClient.getInstance().showSignIn(
+            this@AuthenticatorActivity,
+            SignInUIOptions.builder()
+                .nextActivity(MainActivity::class.java)
+                .build(),
+            object : Callback<UserStateDetails> {
+                override fun onResult(result: UserStateDetails) {
+                    Log.d("showSignIn", result.userState.name)
+                }
+
+                override fun onError(e: Exception) {
+                }
+            }
+        )
+    }
+}
--- app/src/main/java/com/matsuda/chichibu/MainActivity.kt
+++ app/src/main/java/com/matsuda/chichibu/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.matsuda.chichibu
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.databinding.adapters.SearchViewBindingAdapter
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobile.client.UserStateDetails
 import com.matsuda.chichibu.view.navigator.ViewNavigator
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +23,25 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.navigation_dashboard ->
                     ViewNavigator.moveToMyPage(supportFragmentManager)
-                R.id.navigation_login ->
-                    ViewNavigator.moveToLogin(supportFragmentManager)
+                R.id.navigation_login -> {
+//                    ViewNavigator.moveToLogin(supportFragmentManager)
+                    startActivity(Intent(this, AuthenticatorActivity::class.java))
+                }
                 else ->
                     ViewNavigator.moveToHome(supportFragmentManager)
             }
             false
         }
+
+        AWSMobileClient.getInstance()
+            .initialize(applicationContext, object : Callback<UserStateDetails> {
+                override fun onResult(userStateDetails: UserStateDetails) {
+                    Log.i("INIT", "onResult: " + userStateDetails.userState)
+                }
+
+                override fun onError(e: Exception) {
+                    Log.e("INIT", "Initialization error.", e)
+                }
+            })
     }
 }
